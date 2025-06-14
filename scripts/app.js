5029202d76d44f889ffd3b4197adba830d2aa667
MaaikePh <maaikephielix@live.nl>
2025-06-14 14:39:37 +0200

    Opdracht 2 gemaakt.

--- scripts/app.js
+++ scripts/app.js
@@ -16,3 +16,23 @@ console.log("De afdeling Customer Service heeft " + departments["customer-servic
 
 console.log("Sales is een uitdagende afdeling om te werken als Verkoopmanager. " + departments.sales.jobs[1].description);
 
+// Opdracht 2a
+
+const userInput = prompt("Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]");
+console.log(userInput);
+
+// Opdracht 2b & 2C
+
+switch (userInput) {
+    case "marketing":
+        console.log("Je koos" + userInput + ". " + departments.marketing.description);
+        break;
+    case "sales":
+        console.log("Je koos " + userInput + ". " + departments.sales.description);
+        break;
+    case "customer-service":
+        console.log("Je koos " + userInput + ". " + departments["customer-service"].description);
+        break;
+    default:
+        console.error("Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.");
+}
